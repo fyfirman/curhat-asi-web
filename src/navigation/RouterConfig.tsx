8e07624854efc6d9b90c2a7e7e4a22abf9664d5d
Firmansyah Yanuar <fyfirman@gmail.com>
2021-03-23 13:21:55 +0700

    router example

---
+++ src/navigation/RouterConfig.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { Route, Redirect } from "react-router-dom";
+import Login from "@pages/login/Login";
+import Users from "@pages/private/users/Users";
+import Consultations from "@pages/private/consultations/Consultations";
+
+const RouterConfig: React.FC = () => {
+  const [loggedIn, _] = React.useState(false);
+
+  return (
+    <div>
+      Test
+      <Route exact path="/">
+        {loggedIn ? <Redirect to="/user" /> : <Redirect to="/login" />}
+      </Route>
+      <Route path="/login" component={Login} />
+      <Route path="/user" component={Users} />
+      <Route path="/consultation" component={Consultations} />
+    </div>
+  );
+};
+
+export default RouterConfig;
