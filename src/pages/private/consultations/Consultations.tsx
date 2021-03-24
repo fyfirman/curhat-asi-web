6842aa52d1e1199293f24765a363afff6f728706
Firmansyah Yanuar <fyfirman@gmail.com>
2021-03-24 08:11:13 +0700

    feat: create mock interface consultation and user

--- src/pages/private/consultations/Consultations.tsx
+++ src/pages/private/consultations/Consultations.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, Theme } from "@material-ui/core";
+import { DataGrid } from "@material-ui/data-grid";
+import { columns, rows } from "./mock.data";
+
+const useStyle = makeStyles((theme: Theme) => ({
+  container: {
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(4)
+  },
+  header: {
+    marginBottom: theme.spacing(3)
+  },
+  tableContainer: {
+    // height: "80vh"
+  }
+}));
 
 const Consultations = () => {
-  return <div>Consultations Page</div>;
+  const classes = useStyle();
+
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4" className={classes.header}>
+        Daftar Konsultasi
+      </Typography>
+      <DataGrid autoHeight rows={rows} columns={columns} pageSize={20} checkboxSelection={false} />
+    </div>
+  );
 };
 
 export default Consultations;
--- src/pages/private/users/Users.tsx
+++ src/pages/private/users/Users.tsx
@@ -1,22 +1,32 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, Theme } from "@material-ui/core";
+import { DataGrid } from "@material-ui/data-grid";
+import { columns, rows } from "./mock.data";
+
+const useStyle = makeStyles((theme: Theme) => ({
+  container: {
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(4)
+  },
+  header: {
+    marginBottom: theme.spacing(3)
+  },
+  tableContainer: {
+    height: "80vh"
+  }
+}));
 
 const Users = () => {
+  const classes = useStyle();
+
   return (
-    <main>
-      <Grid container spacing={1} direction="row">
-        <Grid item>User Page</Grid>
-        <Grid item>User Page</Grid>
-        <Grid item>User Page</Grid>
-        <Grid item>User Page</Grid>
-        <Grid item>User Page</Grid>
-        <Grid item>User Page</Grid>
-        <Grid item>User Page</Grid>
-        <Grid item>User Page</Grid>
-        <Grid item>User Page</Grid>
-        <Grid item>User Page</Grid>
-      </Grid>
-    </main>
+    <div className={classes.container}>
+      <Typography variant="h4" className={classes.header}>
+        Daftar Pengguna
+      </Typography>
+      <DataGrid autoHeight rows={rows} columns={columns} pageSize={20} checkboxSelection={false} />
+    </div>
   );
 };
 
