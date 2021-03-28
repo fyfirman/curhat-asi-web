9475fa7201740a84dcc8d9a8bdd28a6ff5c6e6f7
Firmansyah Yanuar <fyfirman@gmail.com>
2021-03-28 15:53:18 +0700

    fix: axios logger

--- src/utils/axios/axios.ts
+++ src/utils/axios/axios.ts
@@ -1,29 +1,35 @@
 /* eslint-disable no-console */
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { BASE_URL, DEBUG_MODE } from '@utils/config';
 
 const instance = axios.create({
   baseURL: BASE_URL,
 });
 
+const logger = (config: AxiosRequestConfig) => {
+  console.table([
+    ['method', config.method],
+    ['base_url', config.baseURL],
+    ['url', config.url],
+    ['auth-headers', config.headers?.Authorization],
+    ['body-data', !config.data],
+  ]);
+
+  const formData: FormData = config.data;
+  if (formData) {
+    const body = {};
+    formData.forEach((value, key) => {
+      Object.assign(body, { [key]: value });
+    });
+    console.log(body);
+  }
+};
+
 instance.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 instance.interceptors.request.use(
   (config) => {
     if (DEBUG_MODE) {
-      console.table([
-        ['method', config.method],
-        ['base_url', config.baseURL],
-        ['url', config.url],
-        ['auth-headers', config.headers?.Authorization],
-        ['body-data', !config.data],
-      ]);
-
-      const formData: FormData = config.data;
-      const body = {};
-      formData.forEach((value, key) => {
-        Object.assign(body, { [key]: value });
-      });
-      console.log(body);
+      logger(config);
     }
     return config;
   },
