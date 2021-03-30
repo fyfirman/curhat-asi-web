3922878241107ddc85298d5b736eef55a653bcd5
Firmansyah Yanuar <fyfirman@gmail.com>
2021-03-30 10:35:14 +0700

    feat: create breadcrumbs

--- src/pages/admin/consultation/constultationRoom/ConsultationRoom.tsx
+++ src/pages/admin/consultation/constultationRoom/ConsultationRoom.tsx
@@ -1,34 +1,40 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { requestConsultationPosts } from '@redux/actions/consultationPostActions';
 import { RootState } from '@redux/reducers';
+import { Breadcrumbs, Typography } from '@material-ui/core';
 
 const ConsultationRoom = () => {
   const { id } = useParams<{ id: string }>();
 
   const dispatch = useDispatch();
 
-  const consultationPosts = useSelector(
-    (state: RootState) => state.consultationPosts,
+  const consultation = useSelector(
+    (state: RootState) => state.consultations.payload.data.find(
+      (item) => item.id === id,
+    ),
   );
 
   useEffect(() => {
     dispatch(requestConsultationPosts(parseInt(id, 10)));
   }, []);
 
-  const renderPosts = () => consultationPosts?.data.map((post) => (
+  const renderPosts = () => consultation?.posts.map((post) => (
     <div key={post.id}>{post.message}</div>
   ));
 
-  return consultationPosts?.isLoaded ? (
+  return (
     <div>
-      Consultation id :
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link to="/admin/consultation">
+          Konsultasi
+        </Link>
+        <Typography color="textPrimary">{consultation?.user.fullName || 'Belum mengisi profile'}</Typography>
+      </Breadcrumbs>
       {id}
       {renderPosts()}
     </div>
-  ) : (
-    <div>Loading</div>
   );
 };
 
