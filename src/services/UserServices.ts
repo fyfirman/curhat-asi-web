6ac62f5c0ea0eb939df39a7f1d1610093d25d79e
Firmansyah Yanuar <fyfirman@gmail.com>
2021-04-01 19:55:52 +0700

    feat: create user service

--- src/services/UserServices.ts
+++ src/services/UserServices.ts
@@ -1,16 +1,17 @@
 import get from '@utils/axios/get';
 import * as Config from '@utils/axios/axiosConfig';
 import UserGroup from '@constants/UserGroupEnum';
+import UserGroupRoutes from '@constants/UserGroupRoutes';
 
-export const getUsers = (type?: UserGroup) => {
+export const getUsers = (type: UserGroup) => {
   const config = {
     ...(type && { params: { type } }),
   };
 
-  return get('api/profile', Config.withToken(config));
+  return get(`api/${UserGroupRoutes[type]}`, Config.withToken(config));
 };
 
-export const getUser = (id: IUser['id']) => get(
-  `api/profile/${id}`,
+export const getUser = (type: UserGroup, id: IUser['id']) => get(
+  `api/${UserGroupRoutes[type]}/${id}`,
   Config.withToken(),
 );
