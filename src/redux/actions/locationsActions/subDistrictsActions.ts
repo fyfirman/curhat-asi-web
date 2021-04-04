6b544f9697895bdddeb4113100599d31e202dc7d
Firmansyah Yanuar <fyfirman@gmail.com>
2021-04-05 06:15:24 +0700

    feat: location services

---
+++ src/redux/actions/locationsActions/subDistrictsActions.ts
@@ -0,0 +1,24 @@
+import { subDistrictsTypes } from '@redux/constants/locationsActionTypes';
+import * as LocationServices from '@services/LocationServices';
+
+export const requestVillages = (districtId: IDistrict['id']) => async (dispatch: DispatchType) => {
+  try {
+    const response = await LocationServices.getSubDistricts(districtId);
+
+    dispatch(requestVillagesSuccess(response.payload as ISubDistrict[]));
+  } catch (error) {
+    // TODO:  dispatch() error;
+    // eslint-disable-next-line no-alert
+    alert(JSON.stringify(error));
+    dispatch(requestVillagesFailure());
+  }
+};
+
+export const requestVillagesSuccess = (subDistricts: ISubDistrict[]) => ({
+  type: subDistrictsTypes.FETCH_SUB_DISTRICTS_SUCCESS,
+  payload: subDistricts,
+});
+
+export const requestVillagesFailure = () => ({
+  type: subDistrictsTypes.FETCH_SUB_DISTRICTS_FAILURE,
+});
