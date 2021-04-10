68a7bb0f889fe4f844f1d1ef8eb9e7caaca916ca
Firmansyah Yanuar <fyfirman@gmail.com>
2021-04-10 17:49:56 +0700

    feat: filter tabs

--- src/pages/admin/user/userList/components/UserContent.tsx
+++ src/pages/admin/user/userList/components/UserContent.tsx
@@ -1,21 +1,31 @@
-import React, { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { Tab, Tabs } from '@material-ui/core';
 import UserGroup from '@constants/UserGroupEnum';
+import { RootState } from '@redux/reducers';
+import { useSelector } from 'react-redux';
 import TabPanel from './TabPanel';
 import UserDataGrid from './UserDataGrid';
 
 // TODO: add administrator when API created
-const tabsData: { label: string; type: UserGroup }[] = [
-  { label: 'Ibu', type: UserGroup.Mommies },
-  { label: 'Kader', type: UserGroup.Cadre },
-  { label: 'Bidan', type: UserGroup.Midwife },
-  { label: 'Konselor', type: UserGroup.Conselor },
-  { label: 'Dokter Umum', type: UserGroup.DoctorGeneral },
-  { label: 'Dokter Spesialis', type: UserGroup.DoctorSpecialist },
+const tabsData: { label: string; type: UserGroup, level: number }[] = [
+  { label: 'Ibu', type: UserGroup.Mommies, level: 6 },
+  { label: 'Kader', type: UserGroup.Cadre, level: 5 },
+  { label: 'Bidan', type: UserGroup.Midwife, level: 4 },
+  { label: 'Konselor', type: UserGroup.Conselor, level: 3 },
+  { label: 'Dokter Umum', type: UserGroup.DoctorGeneral, level: 2 },
+  { label: 'Dokter Spesialis', type: UserGroup.DoctorSpecialist, level: 1 },
+  { label: 'Administrator', type: UserGroup.Administrator, level: 0 },
 ];
 
 const UserContent = () => {
   const [value, setValue] = useState(0);
+
+  // eslint-disable-next-line no-underscore-dangle
+  const selfUser = useSelector((state:RootState) => state.selfUser.payload);
+
+  const filteredTab = useMemo(() => tabsData
+    .filter(((tab) => tab.level >= selfUser.userGroup.level)),
+  [selfUser]);
 
   const handleChange = (_event: any, newValue: number) => {
     setValue(newValue);
@@ -26,11 +36,11 @@ const UserContent = () => {
     'aria-controls': `scrollable-auto-tabpanel-${index}`,
   });
 
-  const renderTabs = () => tabsData.map((tab, index) => (
+  const renderTabs = () => filteredTab.map((tab, index) => (
     <Tab label={tab.label} {...a11yProps(index)} />
   ));
 
-  const renderTabPanels = () => tabsData.map((tab, index) => (
+  const renderTabPanels = () => filteredTab.map((tab, index) => (
     <TabPanel value={value} index={index}>
       <UserDataGrid type={tab.type} />
     </TabPanel>
