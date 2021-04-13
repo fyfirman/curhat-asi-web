6157a5b25ead6a07cf469e84cd283645e5601dfa
Firmansyah Yanuar <fyfirman@gmail.com>
2021-04-14 06:42:47 +0700

    feat: create 3 tabs

--- src/pages/admin/article/articleShow/ArticleShow.tsx
+++ src/pages/admin/article/articleShow/ArticleShow.tsx
@@ -1,7 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { ChangeEvent, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { makeStyles, Theme } from '@material-ui/core';
+import { makeStyles, Tabs, Theme } from '@material-ui/core';
 import ContentLayout from '@components/ContentLayout';
+import Tab from '@components/Tab';
+import TabPanel from '@components/TabPanel';
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
@@ -15,6 +18,12 @@ const useStyles = makeStyles((theme: Theme) => ({
 const ArticleShow = () => {
   const classes = useStyles();
 
+  const [value, setValue] = useState(0);
+
+  const handleChange = (_event: ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
+
   const { id } = useParams<{ id: string }>();
 
   return (
@@ -23,7 +32,27 @@ const ArticleShow = () => {
       levelOneTo="/admin/article"
       levelTwoLabel="Cara Memberikan ASI yang Benar"
     >
-      Test
+      <Tabs
+        value={value}
+        indicatorColor="primary"
+        variant="scrollable"
+        scrollButtons="auto"
+        aria-label="tabs article"
+        onChange={handleChange}
+      >
+        <Tab label="Pratinjau" index={0} />
+        <Tab label="Pembaca" index={1} />
+        <Tab label="Penilaian" index={2} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        Preview
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Test 2
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Test 3
+      </TabPanel>
     </ContentLayout>
   );
 };
