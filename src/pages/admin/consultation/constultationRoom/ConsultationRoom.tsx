866b2cc9b388e7c07bfe520fbab0dd97691e2107
Firmansyah Yanuar <fyfirman@gmail.com>
2021-04-14 06:17:35 +0700

    refactor: content layout consultation room

--- src/pages/admin/consultation/constultationRoom/ConsultationRoom.tsx
+++ src/pages/admin/consultation/constultationRoom/ConsultationRoom.tsx
@@ -2,23 +2,11 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { requestConsultation } from '@redux/actions/consultationActions';
-import { makeStyles, Theme } from '@material-ui/core';
-import Breadcrumbs from '@components/CustomBreadcrumbs';
 import { RootState } from '@redux/reducers';
+import ContentLayout from '@components/ContentLayout';
 import ChatContainer from './components/ChatContainer';
 
-const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    gap: theme.spacing(2),
-  },
-}));
-
 const ConsultationRoom = () => {
-  const classes = useStyles();
-
   const { id } = useParams<{ id: string }>();
 
   const dispatch = useDispatch();
@@ -32,14 +20,14 @@ const ConsultationRoom = () => {
   // TODO: add loading when dispatching
 
   return (
-    <div className={classes.root}>
-      <Breadcrumbs
-        levelOneLabel="Konsultasi"
-        levelOneTo="/admin/consultation"
-        levelTwoLabel={`${consultation?.user?.fullName || 'Belum mengisi profile'} ${consultation.description}`}
-      />
+    <ContentLayout
+      levelOneLabel="Konsultasi"
+      levelOneTo="/admin/consultation"
+      levelTwoLabel={`${consultation?.user?.fullName || 'Belum mengisi profile'} ${consultation.description}`}
+      noContainer
+    >
       <ChatContainer />
-    </div>
+    </ContentLayout>
   );
 };
 
