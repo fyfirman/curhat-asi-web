168c30987fdb5e4662f7e7737caff86f00dab7d8
Firmansyah Yanuar <fyfirman@gmail.com>
2021-04-13 19:53:58 +0700

    feat: add checkbox

--- src/pages/admin/article/createArticle/components/AccessByField.tsx
+++ src/pages/admin/article/createArticle/components/AccessByField.tsx
@@ -2,21 +2,25 @@ import UserGroup from '@constants/UserGroupEnum';
 import { $enum } from 'ts-enum-util';
 import { CheckboxWithLabel } from 'formik-material-ui';
 import UserGroupLabel from '@constants/UserGroupLabel';
-import CustomField from './CustomField';
+import { Field } from 'formik';
 
 const AccessByField = () => {
   const renderCheckBox = () => $enum(UserGroup).map((userGroup) => (
-    <CustomField
+    <Field
       key={userGroup}
       component={CheckboxWithLabel}
       type="checkbox"
-      name="checked"
+      name={`checked-${userGroup}`}
       Label={{ label: UserGroupLabel[userGroup] }}
     />
   ));
 
   return (
     <>
+      <span>
+        Dapat dilihat oleh
+      </span>
+      <br />
       { renderCheckBox() }
     </>
   );
