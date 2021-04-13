ffba199838d3e9b7742ce50545a4d1badb302675
Firmansyah Yanuar <fyfirman@gmail.com>
2021-04-14 06:19:22 +0700

    refactor: content layout user profile

--- src/pages/admin/user/userProfile/UserProfile.tsx
+++ src/pages/admin/user/userProfile/UserProfile.tsx
@@ -1,25 +1,16 @@
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import Breadcrumbs from '@components/CustomBreadcrumbs';
-import { makeStyles, Theme } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@redux/reducers';
 import { requestUserProfile } from '@redux/actions/userProfileActions';
 import UserGroup from '@constants/UserGroupEnum';
-import Container from '@components/Container';
+import ContentLayout from '@components/ContentLayout';
 import Header from './components/Header';
 import TabsInfo from './components/TabsInfo';
-
-const useStyles = makeStyles((theme: Theme) => ({
-  breadCrumbs: {
-    marginBottom: theme.spacing(2),
-  },
-}));
 
 const ShowUser = () => {
   const { id } = useParams<{ id: string }>();
 
-  // eslint-disable-next-line no-underscore-dangle
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -28,21 +19,15 @@ const ShowUser = () => {
 
   const user = useSelector((state: RootState) => state.userProfile.payload);
 
-  const classes = useStyles();
-
   return (
-    <div>
-      <Breadcrumbs
-        className={classes.breadCrumbs}
-        levelOneLabel="Pengguna"
-        levelOneTo="/admin/user"
-        levelTwoLabel={user?.fullName || 'Belum mengisi profile'}
-      />
-      <Container>
-        <Header />
-        <TabsInfo consultant={user?.userGroup.id !== UserGroup.Mommies} />
-      </Container>
-    </div>
+    <ContentLayout
+      levelOneLabel="Pengguna"
+      levelOneTo="/admin/user"
+      levelTwoLabel={user?.fullName || 'Belum mengisi profile'}
+    >
+      <Header />
+      <TabsInfo consultant={user?.userGroup.id !== UserGroup.Mommies} />
+    </ContentLayout>
   );
 };
 
