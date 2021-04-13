b4f186ce155b67575b04049d18578d3daf0cc330
Firmansyah Yanuar <fyfirman@gmail.com>
2021-04-13 20:12:31 +0700

    feat: add tags

--- src/pages/admin/article/createArticle/components/TagsField.tsx
+++ src/pages/admin/article/createArticle/components/TagsField.tsx
@@ -1,10 +1,13 @@
 import CustomAutocomplete from '@components/CustomAutocomplete';
+import { createFilterOptions } from '@material-ui/lab/useAutocomplete';
 
 const mockData : { title: string }[] = [
   { title: 'Halo' },
   { title: 'Halo 1' },
   { title: 'Halo 2' },
 ];
+
+const filter = createFilterOptions<{ title: string, inputValue?: string }>();
 
 const TagsField = () => {
   return (
@@ -13,8 +16,28 @@ const TagsField = () => {
       name="tags"
       multiple
       options={mockData}
-      getOptionLabel={({ title }: any) => title}
+      getOptionLabel={(option:any) => {
+        if (typeof option === 'string') {
+          return option;
+        }
+        if (option.inputValue) {
+          return option.inputValue;
+        }
+        return option.title;
+      }}
+      renderOption={(option: any) => option.title}
+      filterOptions={(options: any, params:any) => {
+        const filtered = filter(options, params) as { title: string, inputValue?: string }[];
 
+        if (params.inputValue !== '') {
+          filtered.push({
+            inputValue: params.inputValue,
+            title: `Tambahkan "${params.inputValue}"`,
+          });
+        }
+
+        return filtered;
+      }}
     />
   );
 };
