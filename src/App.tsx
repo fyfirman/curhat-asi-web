0594a1b73137aeae01070ffa056dd7bada298272
Firmansyah Yanuar <fyfirman@gmail.com>
2021-04-16 04:33:41 +0700

    move app and add test

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { CssBaseline } from '@material-ui/core';
 import { Helmet } from 'react-helmet';
-import {
-  ThemeProvider, jssPreset, StylesProvider,
-} from '@material-ui/core/styles';
+import { ThemeProvider, jssPreset, StylesProvider } from '@material-ui/core/styles';
 import theme from '@theme/theme';
 import AppRouter from '@router/Router';
+import { Provider } from 'react-redux';
+import store, { persistor } from '@redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
-import { persistor } from '@redux/store';
 import { create } from 'jss';
 import jssExtends from 'jss-plugin-extend';
 import 'date-fns';
@@ -18,24 +17,26 @@ const jss = create({
 
 export default function App() {
   return (
-    <StylesProvider jss={jss}>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Curhat ASI</title>
-        <link rel="canonical" href="http://mysite.com/example" />
-      </Helmet>
-      <ThemeProvider theme={theme}>
-        <PersistGate
-          loading={<div>Mohon tunggu</div>}
-          persistor={persistor}
-        >
-          <Router>
-            <AppRouter />
-          </Router>
-          <CssBaseline />
-        </PersistGate>
-      </ThemeProvider>
-    </StylesProvider>
+    <Provider store={store}>
+      <StylesProvider jss={jss}>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Curhat ASI</title>
+          <link rel="canonical" href="http://curhatasi.fit" />
+        </Helmet>
+        <ThemeProvider theme={theme}>
+          <PersistGate
+            loading={<div>Mohon tunggu</div>}
+            persistor={persistor}
+          >
+            <Router>
+              <AppRouter />
+            </Router>
+            <CssBaseline />
+          </PersistGate>
+        </ThemeProvider>
+      </StylesProvider>
+    </Provider>
 
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from '@redux/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
