933511c9c92f78b9c3c43f0225249ce8754ee59e
Firmansyah Yanuar <fyfirman@gmail.com>
2021-04-16 04:27:48 +0700

    fix: persiststore

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware } from '@reduxjs/toolkit';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { persistReducer } from 'redux-persist';
-import persistStore from 'redux-persist/es/persistStore';
+import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
