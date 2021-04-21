caef5fbe7f0d0deab0b118b0f213df28dc5551f7
Firmansyah Yanuar <fyfirman@gmail.com>
2021-04-21 11:32:34 +0700

    feat: create download button consultation

--- src/pages/admin/consultation/consultationList/ConsultationList.tsx
+++ src/pages/admin/consultation/consultationList/ConsultationList.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo } from 'react';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles, Theme } from '@material-ui/core';
+import { Button, makeStyles, Theme } from '@material-ui/core';
 import { DataGrid } from '@material-ui/data-grid';
 import { useDispatch, useSelector } from 'react-redux';
 import { requestConsultations } from '@redux/actions/consultationListActions';
@@ -11,12 +11,19 @@ const useStyle = makeStyles((theme: Theme) => ({
   header: {
     marginBottom: theme.spacing(3),
   },
+  headerContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginBottom: theme.spacing(3),
+  },
 }));
 
 const Consultations = () => {
   const classes = useStyle();
   const dispatch = useDispatch();
+
   const consultationList = useSelector((state: RootState) => state.consultationList);
+  const { level } = useSelector((state: RootState) => state.selfUser.payload.userGroup);
 
   useEffect(() => {
     dispatch(requestConsultations());
@@ -35,9 +42,19 @@ const Consultations = () => {
 
   return (
     <>
-      <Typography variant="h4" className={classes.header}>
-        Daftar Konsultasi
-      </Typography>
+      <div className={classes.headerContainer}>
+        <Typography variant="h4" className={classes.header}>
+          Daftar Konsultasi
+        </Typography>
+        {level === 0 && (
+          <div>
+            <Button variant="contained" color="secondary">
+              Download Excel
+            </Button>
+          </div>
+        )}
+      </div>
+
       <DataGrid autoHeight rows={rows} columns={columns} pageSize={20} checkboxSelection={false} />
     </>
   );
